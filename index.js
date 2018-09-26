4c34c7b86d6ee26e8eeb19c2c10b06013efd6309
gym487 <gym487@163.com>
2018-09-26 22:31:17 +0800

    tst2

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 var express=require("express");
 var app=express();
+var fs=require('fs');
 
+app.get('/files/*', function(req, res) {   
+var pathname = url.parse(req.url).pathname;
+   console.log("Request for " + pathname + " received.");
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         fs.readFile("./files/404.html",function(err,dataa){
+			 if(!err){
+					res.writeHead(404, {'Content-Type': 'text/html'});
+					res.write(dataa);	
+					res.end();
+				}
+			});
+      }else{	         
+var ext = path.extname(pathname);
+ext = ext ? ext.slice(1) : 'unknown';
+var contentType = mime[ext] || "text/plain";
+res.writeHead(200, {'Content-Type': contentType});
+res.write(data);
+res.end();
+}
+});
+});
 app.get('/', function (req, res) {
    res.send("<html><head><title>TEST</title></head><body><h1>TEST</h1></body></html>");
-})
- 
+}) 
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
