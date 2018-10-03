8bc71cee718b3b7ab9443f7dff97db6cfd3cd05f
gym487 <gym487@163.com>
2018-10-04 02:25:37 +0800

    navga

--- statics/map.js
+++ statics/map.js
@@ -1,10 +1,16 @@
 var mp;
 
 function newMap(c){
-					mp = new BMap.Map(c);
-                    var point = new BMap.Point(116.404,39.915);
-                    mp.centerAndZoom(point,5);
-                    mp.enableScrollWheelZoom(true);
+	mp = new BMap.Map(c);
+    var point = new BMap.Point(116.404,39.915);
+   	var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});
+	//var top_left_navigation = new BMap.NavigationControl();
+	var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); 
+    mp.centerAndZoom(point,5);
+    mp.enableScrollWheelZoom(true);
+    mp.addControl(top_left_control);        
+	//mp.addControl(top_left_navigation);     
+	mp.addControl(top_right_navigation); 
 }
 function label(lon,lat,str){
 	var point=new BMap.Point(lon,lat);
@@ -21,3 +27,4 @@ function mclick(f){
 function mclear(){
 	mp.clearOverlays();
 }
+
